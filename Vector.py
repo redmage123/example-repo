386a53dbac2694aba69c70d6702fd22421d86e7d
bbrelin <bbrelin@gmail.com>
2021-08-10 06:47:31 +0300

    This is the initial commit for this git repo

---
+++ Vector.py
@@ -0,0 +1,16 @@
+class Vector:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return 'x: ' + str(self.x) + 'y: ' + str(self.y)
+
+    def __add__(self,other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+# Only this new code will be saved in the commit in deltas
+    def __sub__(self,other):
+        return Vector(self.x - other.x, self.y - other.y)
+
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import Vector
+
+v = Vector(1,2)
+v1 = Vector (2,3)
+v3 = v1 + v2
+
+print (v3)
